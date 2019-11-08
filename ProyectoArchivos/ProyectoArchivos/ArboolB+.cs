3e2446401946fa26e119a3bd71e090ed04f9a2bf
Alejandro <Alejandro@DESKTOP-B44CUKP>
2019-11-08 16:49:41 -0600

    Inicio de ArbolB+

--- ProyectoArchivos/ProyectoArchivos/ArboolB+.cs
+++ ProyectoArchivos/ProyectoArchivos/ArboolB+.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,23 @@ namespace ProyectoArchivos
 {
     class ArboolB_
     {
+        public FileStream archivo;
+        public String nombreArch;
+        private int grado;
+        private NodoArbol raiz;
+        private List<NodoArbol> hojas;
+        private List<NodoArbol> intermedios;
+        private int dir;
+
+        public ArboolB_(int g, String name)
+        {
+            dir = 0;
+            nombreArch = name;
+            grado = g;
+            raiz = null;
+            hojas = new List<NodoArbol>();
+            intermedios = new List<NodoArbol>();
+        }
 
     }
 }
--- ProyectoArchivos/ProyectoArchivos/NodoArbol.cs
+++ ProyectoArchivos/ProyectoArchivos/NodoArbol.cs
@@ -8,6 +8,69 @@ namespace ProyectoArchivos
 {
     class NodoArbol
     {
+        private char tipo;
+        private int longitudCve;
+        private Int64 DirNodo;
+        private List<Int64> Apuntadores;
+        private List<String> claves;
+        private List<byte[]> nodoBytes;
 
+        public NodoArbol(char t,Int64 dir, int g,int tam)
+        {
+            tipo = t;
+            longitudCve = tam;
+            DirNodo = dir;
+            nodoBytes = new List<byte[]>();
+            Apuntadores = new List<Int64>();
+            claves = new List<String>();
+            inicializaListas(g);
+            inicializaBytes(g);
+        }
+        public void inicializaListas(int grado)
+        {
+            Apuntadores.Add(-1);
+            for(int i =0; i < grado - 1; i++)
+            {
+                Apuntadores.Add(-1);
+                claves.Add("");
+            }
+        }
+        public void inicializaBytes(int grado)
+        {
+            nodoBytes.Add(new byte[1]);
+            nodoBytes.Add(new byte[8]);
+            nodoBytes.Add(new byte[8]);
+            for (int i = 0; i < grado - 1; i++)
+            {
+                nodoBytes.Add(new byte[longitudCve]);
+                nodoBytes.Add(new byte[8]);
+            }
+        }
+        public void actualizaBytes()
+        {
+            int cnt = 2;
+            Encoding.ASCII.GetBytes(tipo.ToString(),0, 1, nodoBytes[0], 0);
+            Encoding.ASCII.GetBytes(Apuntadores[0].ToString(), 0, Apuntadores[0].ToString().Length, nodoBytes[1], 0);
+            for(int i =0; i < claves.Count; i++)
+            {
+                Encoding.ASCII.GetBytes(claves[i].ToString(), 0, claves[i].ToString().Length, nodoBytes[cnt], 0);
+                cnt++;
+                Encoding.ASCII.GetBytes(Apuntadores[i+1].ToString(), 0, Apuntadores[0].ToString().Length, nodoBytes[cnt], 0);
+                cnt++;
+            }
+        }
+        public void actualizaDatos()
+        {
+            int cnt = 2;
+            tipo = Convert.ToChar(Encoding.ASCII.GetString(nodoBytes[0]));
+            DirNodo = Convert.ToInt64(Encoding.ASCII.GetString(nodoBytes[1]));
+            for (int i = 0; i < claves.Count; i++)
+            {
+                claves[cnt] = Encoding.ASCII.GetString(nodoBytes[cnt]);
+                cnt++;
+                Apuntadores[cnt] = Convert.ToInt64(Encoding.ASCII.GetString(nodoBytes[cnt]));
+                cnt++;
+            }
+        }
     }
 }
